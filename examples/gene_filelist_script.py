2b492542b7c3387f9f57164c19057be90fbeb95c
alexandor91 <kangxueyang@126.com>
2021-08-12 12:24:15 +0200

    generate per class name file script finished, useful tool

--- examples/gene_filelist_script.py
+++ examples/gene_filelist_script.py
@@ -5,6 +5,8 @@ import os
 import sys
 import logging
 import decimal
+import glob
+
 
 def gene_filetxt(class_name):
 
@@ -21,37 +23,56 @@ def gene_filetxt(class_name):
     # os.path.splitext(f)
     # os.path.splitext(os.path.basename(f))
 
-    root = "./ModelNet40/"
-    train_percentile = 0.85
+    root = "/home/eavise/MinEngineDeve/ModelNet40"
+    train_percentile = 0.80
     valid_percentile = 1-train_percentile
-    fnames = glob.glob(os.path.join(root, class_name, "/train/*.off"))
+    readpath1 = os.path.join(root, class_name, "train/*.off")
+    readpath2 = os.path.join(root, class_name, "test/*.off")
+    #print(tpath)
+    fnames = glob.glob(readpath1)
     fnames = sorted([os.path.relpath(fname, root) for fname in fnames])
     files = fnames
     assert len(files) > 0, "No file loaded"
     logging.info(
-        f"Loading the files from {root} with {len(files)} files"
+        f"Loading the train files from {root} with {len(files)} files"
     )
     counter = 0
-    write_path1 =  os.path.join(root, "test_", class_name, ".txt" )
-    write_path2 =  os.path.join(root, "train_", class_name, ".txt" )
-    write_path3 =  os.path.join(root, "valid_", class_name, ".txt" )
+    write_path1 =  os.path.join(root, "test_" + class_name + ".txt" )
+    #print(write_path1)
+    write_path2 =  os.path.join(root, "train_" + class_name + ".txt" )
+    write_path3 =  os.path.join(root, "valid_" + class_name  + ".txt" )
     switch_file_flag = False
-    with open(write_path2, 'w') as f:    
-        for fname in fnames:
-            counter =counter+1
-            if decimal.Decimal(counter) / decimal.Decimal(len(files)) <= train_percentile:
-                f.write(fname)
-                f.write('\n')
-            elif (decimal.Decimal(counter) / decimal.Decimal(len(files)) > train_percentile) and (not switch_file_flag):
-                f.close()
-                with open(write_path3, 'w') as f: 
-                    f.write(fname)
-                    f.write('\n')   
-                switch_file_flag = True
-            elif  (decimal.Decimal(counter) / decimal.Decimal(len(files)) > train_percentile) and (switch_file_flag):
-                f.write(fname)
-                f.write('\n')               
+    # splitting the traing dataset into two part, one for traning, one for validation
+    f = open(write_path2, 'w')    
+    for fname in fnames:
+        counter =counter+1
+        if decimal.Decimal(counter) / decimal.Decimal(len(files)) <= train_percentile:
+            f.write(fname)
+            f.write('\n')
+        elif (decimal.Decimal(counter) / decimal.Decimal(len(files)) > train_percentile) and (not switch_file_flag):
+            f.close()
+            f = open(write_path3, 'w')
+            f.write(fname)
+            f.write('\n')   
+            switch_file_flag = True
+        elif  (decimal.Decimal(counter) / decimal.Decimal(len(files)) > train_percentile) and (switch_file_flag):
+            f.write(fname)
+            f.write('\n')               
     f.close()
+
+    fnames = glob.glob(readpath2)
+    fnames = sorted([os.path.relpath(fname, root) for fname in fnames])
+    files = fnames
+    assert len(files) > 0, "No file loaded"
+    logging.info(
+        f"Loading the test files from {root} with {len(files)} files"
+    )
+    f = open(write_path1, 'w')    
+    for fname in fnames:
+        f.write(fname)
+        f.write('\n')    
+    f.close()
+
 
 if __name__ == "__main__":
    gene_filetxt("desk")
